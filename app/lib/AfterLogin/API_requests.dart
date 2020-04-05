25f323f4fd0f87d82d74d95c8b6d36fc76235c79
Gavinkaa <ludovic.burnier@epfl.ch>
2020-04-05 15:50:57 +0200

    api request struct added

---
+++ app/lib/AfterLogin/API_requests.dart
@@ -0,0 +1,40 @@
+class APIRequests {
+  static String GET_listOfRequestsAll() {
+    // List of requests
+    return null;
+  }
+
+  static String GET_listOfRequestsAccepted() {
+    // List of requests
+    return null;
+  }
+
+  static String GET_listOfRequestsFree() {
+    // List of requests
+    return null;
+  }
+
+  static bool POST_request(String json) {
+    return null;
+  }
+
+  static String GET_orderByID(String id) {
+    return null;
+  }
+
+  static String GET_ownContact() {
+    return null;
+  }
+
+  static bool POST_modifyContact(String jsonContact) {
+    return null;
+  }
+
+  static String GET_userType() {
+    return null;
+  }
+
+  static bool POST_acceptOrder(String id) {
+    return null;
+  }
+}
