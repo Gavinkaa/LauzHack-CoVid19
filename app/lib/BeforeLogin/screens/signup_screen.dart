6cb70dc44b90dc9da2cd2c9664c4d9ebbafb14b9
ClementSicard <clement.sicard@epfl.ch>
2020-04-05 22:10:31 +0200

    ça marche presque zebi

--- app/lib/BeforeLogin/screens/signup_screen.dart
+++ app/lib/BeforeLogin/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app/BeforeLogin/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SignupScreen extends StatefulWidget {
   static final String id = 'signup_sceen';
@@ -19,6 +20,7 @@ class _SignupScreenState extends State<SignupScreen> {
       _aptfloor,
       _pcode,
       _city;
+  bool _passwordInvisible, _helps;
 
   _submit() {
     if (_formKey.currentState.validate()) {
@@ -27,6 +29,12 @@ class _SignupScreenState extends State<SignupScreen> {
       AuthServ.signUpUser(context, _firstName, _lastName, _email, _password,
           _telephone, _type, _street, _aptfloor, _pcode, _city);
     }
+  }
+
+  @override
+  void initState() {
+    _passwordInvisible = true;
+    super.initState();
   }
 
   @override
@@ -61,13 +69,13 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'First Name',
+                      labelText: 'Prénom',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _firstName,
                   validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid name' : null,
+                      input.trim().isEmpty ? 'Entrez un prénom valide' : null,
                   onChanged: (input) => setState(() {
                     _firstName = input;
                   }),
@@ -81,13 +89,13 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Last Name',
+                      labelText: 'Nom',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _lastName,
                   validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid name' : null,
+                      input.trim().isEmpty ? 'Entrez un nom valide' : null,
                   onChanged: (input) => setState(() {
                     _lastName = input;
                   }),
@@ -106,8 +114,9 @@ class _SignupScreenState extends State<SignupScreen> {
                         color: Colors.grey[600],
                       )),
                   initialValue: _email,
+                  keyboardType: TextInputType.emailAddress,
                   validator: (input) =>
-                      !input.contains('@') ? 'Please Enter valid email' : null,
+                      !input.contains('@') ? 'Entrez un email valide' : null,
                   onChanged: (input) => setState(() {
                     _email = input;
                   }),
@@ -121,17 +130,30 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Password',
+                      labelText: 'Mot de passe',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
-                      )),
+                      ),
+                      suffixIcon: IconButton(
+                          icon: Icon(
+                            _passwordInvisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.black,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _passwordInvisible = !_passwordInvisible;
+                            });
+                          })),
                   initialValue: _password,
-                  validator: (input) =>
-                      input.length < 6 ? 'Must be at least 6 chars' : null,
+                  validator: (input) => input.length < 6
+                      ? 'Doit faire au moins 6 caractères'
+                      : null,
                   onChanged: (input) => setState(() {
                     _password = input;
                   }),
-                  obscureText: true,
+                  obscureText: _passwordInvisible,
                 ),
               ),
               Padding(
@@ -142,40 +164,63 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Telephone',
+                      labelText: 'Téléphone',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _telephone,
+                  keyboardType: TextInputType.phone,
                   validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid number' : null,
+                      input.trim().isEmpty ? 'Entrez un numéro valide' : null,
                   onChanged: (input) => setState(() {
                     _telephone = input;
                   }),
                 ),
               ),
               Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 30.0,
-                  vertical: 5.0,
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Type: Helper/Asker',
-                      labelStyle: TextStyle(
-                        color: Colors.grey[600],
-                      )),
-                  initialValue: _type,
-                  validator: (input) =>
-                      !(input.trim() == "Asker" || input.trim() == "Helper")
-                          ? 'Please type Helper or Asker'
-                          : null,
-                  onChanged: (input) => setState(() {
-                    _type = input;
-                  }),
-                ),
-              ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                    vertical: 5.0,
+                  ),
+                  child:
+                      // TextFormField(
+                      //   decoration: InputDecoration(
+                      //       contentPadding: EdgeInsets.all(10.0),
+                      //       labelText: 'Type: Helper/Asker',
+                      //       labelStyle: TextStyle(
+                      //         color: Colors.grey[600],
+                      //       )),
+                      //   initialValue: _type,
+                      //   validator: (input) =>
+                      //       !(input.trim() == "Asker" || input.trim() == "Helper")
+                      //           ? "Entrez 'Asker' ou 'Helper'"
+                      //           : null,
+                      //   onChanged: (input) => setState(() {
+                      //     _type = input;
+                      //   }),
+                      // ),
+                      Row(children: <Widget>[
+                    RadioListTile<String>(
+                      title: const Text("Demandeur d'aide"),
+                      value: 'Asker',
+                      groupValue: _type,
+                      onChanged: (value) {
+                        setState(() {
+                          _type = value;
+                        });
+                      },
+                    ),
+                    RadioListTile<String>(
+                      title: const Text("Aideur"),
+                      value: 'Helper',
+                      groupValue: _type,
+                      onChanged: (value) {
+                        setState(() {
+                          _type = value;
+                        });
+                      },
+                    ),
+                  ])),
               Padding(
                 padding: EdgeInsets.symmetric(
                   horizontal: 30.0,
@@ -184,13 +229,13 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Street',
+                      labelText: 'Rue',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _street,
                   validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid name' : null,
+                      input.trim().isEmpty ? 'Entrez une adresse valide' : null,
                   onChanged: (input) => setState(() {
                     _street = input;
                   }),
@@ -204,13 +249,14 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Apt/floor',
+                      labelText: "Complément d'adresse",
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _aptfloor,
-                  validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid name' : null,
+                  validator: (input) => input.trim().isEmpty
+                      ? "Entrez un complément d'adresse valide"
+                      : null,
                   onChanged: (input) => setState(() {
                     _aptfloor = input;
                   }),
@@ -222,15 +268,17 @@ class _SignupScreenState extends State<SignupScreen> {
                   vertical: 5.0,
                 ),
                 child: TextFormField(
+                  keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'Postal Code',
+                      labelText: 'Code postal',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _pcode,
-                  validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid name' : null,
+                  validator: (input) => input.trim().isEmpty
+                      ? 'Entrez un code postal valide'
+                      : null,
                   onChanged: (input) => setState(() {
                     _pcode = input;
                   }),
@@ -244,13 +292,13 @@ class _SignupScreenState extends State<SignupScreen> {
                 child: TextFormField(
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.all(10.0),
-                      labelText: 'City',
+                      labelText: 'Ville',
                       labelStyle: TextStyle(
                         color: Colors.grey[600],
                       )),
                   initialValue: _city,
                   validator: (input) =>
-                      input.trim().isEmpty ? 'Please Enter valid name' : null,
+                      input.trim().isEmpty ? 'Entrez une ville valide' : null,
                   onChanged: (input) => setState(() {
                     _city = input;
                   }),
@@ -267,7 +315,7 @@ class _SignupScreenState extends State<SignupScreen> {
                     borderRadius: new BorderRadius.circular(8.0),
                   ),
                   child: Text(
-                    'Signup',
+                    "S'inscrire",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 18.0,
@@ -286,7 +334,7 @@ class _SignupScreenState extends State<SignupScreen> {
                     borderRadius: new BorderRadius.circular(8.0),
                   ),
                   child: Text(
-                    'Back to Login',
+                    "Retour",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 18.0,
