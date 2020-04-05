b69dc740bbc2b84dd0d01922a8f2e3e37aecd32c
Gavinkaa <ludovic.burnier@epfl.ch>
2020-04-05 09:25:16 +0200

    Add class Request + class contact

---
+++ app/lib/Contact.dart
@@ -0,0 +1,73 @@
+class Contact {
+  int _pcode;
+  String _city, _street, _aptFloor;
+  String _firstName, _lastName;
+  String _phone;
+  String _email;
+  Contact(this._pcode, this._city, this._street, this._aptFloor,
+      this._firstName, this._lastName, this._phone, this._email);
+
+  int getPCode() {
+    return _pcode;
+  }
+
+  String getCity() {
+    return _city;
+  }
+
+  String getStreet() {
+    return _street;
+  }
+
+  String getAptFloor() {
+    return _aptFloor;
+  }
+
+  String getFirstName() {
+    return _firstName;
+  }
+
+  String getLastname() {
+    return _lastName;
+  }
+
+  String getPhone() {
+    return _phone;
+  }
+
+  String getEmail() {
+    return _email;
+  }
+
+  int setPCode(int pcode) {
+    _pcode = pcode;
+  }
+
+  String setCity(String city) {
+    _city = city;
+  }
+
+  String setStreet(String street) {
+    _street = street;
+  }
+
+  String setAptFloor(String aptFloor) {
+    _aptFloor = aptFloor;
+  }
+
+  String setFirstName(String firstName) {
+    _firstName = firstName;
+  }
+
+  String setLastname(String lastName) {
+    _lastName = lastName;
+  }
+
+  String setPhone(String phone) {
+    _phone = phone;
+  }
+
+  String setEmail(String email) {
+    _email = email;
+  }
+}
---
+++ app/lib/Request.dart
@@ -0,0 +1,8 @@
+import 'package:app/AskSide/Articles.dart';
+
+import 'Contact.dart';
+
+class Request {
+  List<Article> articles;
+  Contact asker;
+}
