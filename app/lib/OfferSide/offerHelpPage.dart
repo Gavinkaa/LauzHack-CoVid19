59b9e8511bd31f2e3aef195d69ffbf3e2e75823e
Gavinkaa <ludovic.burnier@epfl.ch>
2020-04-05 10:19:31 +0200

    request widget added

--- app/lib/OfferSide/offerHelpPage.dart
+++ app/lib/OfferSide/offerHelpPage.dart
@@ -1,4 +1,5 @@
 import 'package:app/AskSide/Articles.dart';
+import 'package:app/Contact.dart';
 import 'package:flutter/material.dart';
 import '../Request.dart';
 
@@ -18,28 +19,44 @@ class _OfferHelpPageState extends State<OfferHelpPage> {
     super.initState();
     _requests = [
       // API calls to database
-      // Request(user name, location, List<Article> articles)
-      // Request("Clément", "Saint-Sulpice", 1),
-      // Request("Lancelot", "Saint-Sulpice", 20),
-      // Request("Douglas", "Morges", 30),
-      // Request("Ludovic", "Lausanne", 10),
-      // Request("Mohamed", "Le Caire", 5),
-      // Request("Clément", "Saint-Sulpice", 1),
-      // Request("Lancelot", "Saint-Sulpice", 20),
-      // Request("Douglas", "Morges", 30),
-      // Request("Ludovic", "Lausanne", 10),
-      // Request("Mohamed", "Le Caire", 5),
-      // Request("Clément", "Saint-Sulpice", 1),
-      // Request("Lancelot", "Saint-Sulpice", 20),
-      // Request("Douglas", "Morges", 30),
-      // Request("Ludovic", "Lausanne", 10),
-      // Request("Mohamed", "Le Caire", 5),
-      // Request("Clément", "Saint-Sulpice", 1),
-      // Request("Lancelot", "Saint-Sulpice", 20),
-      // Request("Douglas", "Morges", 30),
-      // Request("Ludovic", "Lausanne", 10),
-      // Request("Mohamed", "Le Caire", 5)
+      Request(
+          [
+            Article(
+                ArticleType.fromString("Viande"), "beauf", "douglas", 2, ""),
+            Article(ArticleType.fromString("Viande"), "bacon", "luke", 3, "")
+          ],
+          Contact(1055, "Froideville", "Rue du village", "c2", "lancelot",
+              "scheid", "+41 77564", "blabla@gmail.com")),
+      Request(
+          [
+            Article(
+                ArticleType.fromString("Viande"), "beauf", "douglas", 2, ""),
+            Article(ArticleType.fromString("Viande"), "bacon", "luke", 3, "")
+          ],
+          Contact(1055, "Froideville", "Rue du village", "c2", "francois",
+              "scheid", "+41 77564", "blabla@gmail.com"))
     ];
+    // Request(user name, location, List<Article> articles)
+    // Request("Clément", "Saint-Sulpice", 1),
+    // Request("Lancelot", "Saint-Sulpice", 20),
+    // Request("Douglas", "Morges", 30),
+    // Request("Ludovic", "Lausanne", 10),
+    // Request("Mohamed", "Le Caire", 5),
+    // Request("Clément", "Saint-Sulpice", 1),
+    // Request("Lancelot", "Saint-Sulpice", 20),
+    // Request("Douglas", "Morges", 30),
+    // Request("Ludovic", "Lausanne", 10),
+    // Request("Mohamed", "Le Caire", 5),
+    // Request("Clément", "Saint-Sulpice", 1),
+    // Request("Lancelot", "Saint-Sulpice", 20),
+    // Request("Douglas", "Morges", 30),
+    // Request("Ludovic", "Lausanne", 10),
+    // Request("Mohamed", "Le Caire", 5),
+    // Request("Clément", "Saint-Sulpice", 1),
+    // Request("Lancelot", "Saint-Sulpice", 20),
+    // Request("Douglas", "Morges", 30),
+    // Request("Ludovic", "Lausanne", 10),
+    // Request("Mohamed", "Le Caire", 5)
   }
 
   @override
@@ -55,57 +72,7 @@ class _OfferHelpPageState extends State<OfferHelpPage> {
           padding: EdgeInsets.all(5.0),
           child: GestureDetector(
             onTap: () {},
-            child: Card(
-                elevation: 9.0,
-                color: Colors.transparent,
-                margin:
-                    new EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    borderRadius: BorderRadius.all(Radius.circular(6.0)),
-                  ),
-                  child: ListTile(
-                    dense: true,
-                    contentPadding: EdgeInsets.symmetric(horizontal: 3.0),
-                    leading: Column(
-                      children: <Widget>[
-                        Text(
-                          _requests[index].getAsker().getFirstName(),
-                          // textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                            _requests[index].getNumberOfArticles() == 1
-                                ? "1 article"
-                                : _requests[index]
-                                        .getNumberOfArticles()
-                                        .toString() +
-                                    " articles",
-                            // textAlign: TextAlign.left,
-                            style: TextStyle(
-                                fontSize: 10.0, fontWeight: FontWeight.w200))
-                      ],
-                    ),
-                    title: Text(
-                      _requests[index].getAsker().getCity(),
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w200),
-                    ),
-                    trailing: Icon(
-                      Icons.keyboard_arrow_right,
-                      color: Colors.black,
-                      size: 30.0,
-                    ),
-                  ),
-                )),
+            child: _requests[index].widget(),
           ),
         ),
       ),
--- app/lib/Request.dart
+++ app/lib/Request.dart
@@ -1,14 +1,15 @@
 import 'package:app/AskSide/Articles.dart';
+import 'package:flutter/material.dart';
 import 'Contact.dart';
 
 class Request {
   List<Article> _articles;
   int _nbOfArticles;
-  Contact asker;
+  Contact _asker;
 
   Request(List<Article> articles, Contact asker) {
     this._articles = List.from(articles);
-    this.asker = Contact.from(asker);
+    this._asker = Contact.from(asker);
     this._nbOfArticles = _articles.length;
   }
 
@@ -21,7 +22,7 @@ class Request {
   }
 
   Contact getAsker() {
-    return Contact.from(asker);
+    return Contact.from(_asker);
   }
 
   void setArticles(List<Article> articles) {
@@ -30,6 +31,56 @@ class Request {
   }
 
   void setAsker(Contact asker) {
-    this.asker = Contact.from(asker);
+    this._asker = Contact.from(asker);
+  }
+
+  Widget widget() {
+    return Card(
+      elevation: 9.0,
+      color: Colors.transparent,
+      margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+        ),
+        child: ListTile(
+          dense: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 3.0),
+          leading: Column(
+            children: <Widget>[
+              Text(
+                _asker.getFirstName(),
+                // textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                  _nbOfArticles == 1
+                      ? "1 article"
+                      : _nbOfArticles.toString() + " articles",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w200))
+            ],
+          ),
+          title: Text(
+            _asker.getCity(),
+            textAlign: TextAlign.right,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w200),
+          ),
+          trailing: Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.black,
+            size: 30.0,
+          ),
+        ),
+      ),
+    );
   }
 }
