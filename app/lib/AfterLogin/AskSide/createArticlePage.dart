a79a303e82e90ebca72f8b70ac6644025a4ea0c8
ClementSicard <clement.sicard@epfl.ch>
2020-04-06 14:04:43 +0200

    ajout du bouton pour le nouvel article

---
+++ app/lib/AfterLogin/AskSide/createArticlePage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../API_requests.dart';
+import '../Article.dart';
+
+class createArticlePage extends StatefulWidget {
+  createArticlePage({Key key}) : super(key: key);
+
+  @override
+  _createArticlePageState createState() => _createArticlePageState();
+}
+
+class _createArticlePageState extends State<createArticlePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton.extended(
+            backgroundColor: Colors.green,
+            onPressed: () => Navigator.of(context).pop(),
+            icon: new Icon(Icons.save, color: Colors.white),
+            label: Text("SAUVEGARDER",
+                style: TextStyle(
+                    fontWeight: FontWeight.w300, color: Colors.white))),
+        appBar: AppBar(
+          leading: new IconButton(
+            icon: new Icon(Icons.arrow_back_ios, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text('CRÉER UN NOUVEL ARTICLE',
+              style: TextStyle(fontWeight: FontWeight.w300)),
+          centerTitle: true,
+        ));
+  }
+}
