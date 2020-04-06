007b830333f61355cbc596fffa224a2d5640bc16
ClementSicard <clement.sicard@epfl.ch>
2020-04-06 04:20:02 +0200

    ajout askHelpPage

--- app/lib/AfterLogin/AskSide/askHelpPage.dart
+++ app/lib/AfterLogin/AskSide/askHelpPage.dart
@@ -68,8 +68,7 @@ class _AskHelpPageState extends State<AskHelpPage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.baseline,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             RaisedButton(
               onPressed: () {
