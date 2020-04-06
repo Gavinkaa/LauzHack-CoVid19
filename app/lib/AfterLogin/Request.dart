b0f5434dc61fa911a0b98c1fef1e7cc7ff171b49
Gavinkaa <ludovic.burnier@epfl.ch>
2020-04-06 11:51:54 +0200

    request widget for asker + help offer can see his request

--- app/lib/AfterLogin/Request.dart
+++ app/lib/AfterLogin/Request.dart
@@ -40,6 +40,62 @@ class Request {
     this._asker = Contact.from(asker);
   }
 
+  Widget widgetAsker() {
+    return Card(
+      elevation: 4.0,
+      color: Colors.transparent,
+      margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 10),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+        ),
+        child: ListTile(
+          dense: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 3.0),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Ma demande",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                _nbOfArticles == 1
+                    ? "1 article"
+                    : _nbOfArticles.toString() + " articles",
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w200),
+              ),
+            ],
+          ),
+          trailing: Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.black,
+            size: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget widget() {
     return Card(
       elevation: 4.0,
